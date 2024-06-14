b810992abcd7ef00837305c3ddd5c2b956e1fa54
amiro <amiralasady107@gmail.com>
2024-06-14 13:43:47 +0300

    v0.5

--- root_config.py
+++ root_config.py
@@ -4,5 +4,5 @@ config={
     "mode_name_offline":'llama3',                                   # offline local model name
     "api_key_offline":'http://localhost:11434',                 # offline local endpoint
     "context_window":50,         # context window "as bigger as better but takes a lot of resources *not recommended for low-end devices to set above 50*"
-    "root_ip_host":'192.168.1.107'      # root ip *change this to local lan ip for LAN usage or general deployment ip*
+    "root_ip_host":'127.0.0.1'      # root ip *change this to local lan ip for LAN usage or general deployment ip*
 }
